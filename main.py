41c812408d963b3e80d4b810728a187990f17e12
Jone <eugeni.zhidkov@yandex.ru>
2024-04-28 23:21:55 +1000

    четвертый

--- main.py
+++ main.py
@@ -2,10 +2,15 @@ import telebot
 from telebot import types
 import requests
 
+
 API_KEY = '4de6ee8e61214f0182e130103242804'
+
+
 bot = telebot.TeleBot('7138618116:AAHMWU4rmmD-W48pIecOrKBxAl6z6D4ZEbI')
 
-cities = ["Moscow", "London", "Paris", "Berlin", "New York", "Tokyo"]
+
+cities = ["Москва", "Лондон", "Париж", "Берлин", "Нью-Йорк", "Токио"]
+
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
@@ -13,6 +18,7 @@ def send_welcome(message):
     for city in cities:
         markup.add(types.KeyboardButton(city))
     bot.reply_to(message, "Привет! Я бот, который дает информацию о погоде. Выбери город из списка ниже:", reply_markup=markup)
+
 
 @bot.message_handler(func=lambda message: message.text in cities)
 def get_weather(message):
@@ -25,13 +31,14 @@ def get_weather(message):
     else:
         bot.reply_to(message, "Извините, не удалось получить информацию о погоде для этого города.")
 
+
 def fetch_weather(city):
-    url = f'http://api.weatherapi.com/v1/current.json?key={API_KEY}&q={city}'
+    url = f'http://api.weatherapi.com/v1/current.json?key={API_KEY}&q={city}&lang=ru'
     response = requests.get(url)
     if response.status_code == 200:
         return response.json()
     else:
         return None
 
-# Запуск бота
 bot.polling()
+
