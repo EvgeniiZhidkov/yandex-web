83acdebb5784885f51db3e3f29d938bb35f03aa3
Jone <eugeni.zhidkov@yandex.ru>
2024-04-28 23:15:50 +1000

    second commit

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
-# This is a sample Python script.
+import telebot
+import requests
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+API_KEY = '4de6ee8e61214f0182e130103242804'
 
+bot = telebot.TeleBot('7138618116:AAHMWU4rmmD-W48pIecOrKBxAl6z6D4ZEbI')
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "Привет! Я бот, который дает информацию о погоде. Просто отправь мне название города.")
 
+@bot.message_handler(func=lambda message: True)
+def get_weather(message):
+    city = message.text
+    weather_data = fetch_weather(city)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    if weather_data:
+        reply = f"Текущая погода в {city}: {weather_data['current']['condition']['text']}, температура {weather_data['current']['temp_c']}°C, ощущается как {weather_data['current']['feelslike_c']}°C."
+        bot.reply_to(message, reply)
+    else:
+        bot.reply_to(message, "Извините, не удалось получить информацию о погоде для этого города.")
+def fetch_weather(city):
+    url = f'http://api.weatherapi.com/v1/current.json?key={API_KEY}&q={city}'
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None
+bot.polling()
