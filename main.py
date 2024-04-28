23b99e3ca4a1b6609c10b0f7a688d2888a95ff55
Jone <eugeni.zhidkov@yandex.ru>
2024-04-28 23:30:33 +1000

    пятый

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
+#7138618116:AAHMWU4rmmD-W48pIecOrKBxAl6z6D4ZEbI
+#4de6ee8e61214f0182e130103242804
+
 import telebot
 from telebot import types
 import requests
 
-
 API_KEY = '4de6ee8e61214f0182e130103242804'
-
 
 bot = telebot.TeleBot('7138618116:AAHMWU4rmmD-W48pIecOrKBxAl6z6D4ZEbI')
 
-
 cities = ["Москва", "Лондон", "Париж", "Берлин", "Нью-Йорк", "Токио"]
-
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
@@ -19,18 +18,23 @@ def send_welcome(message):
         markup.add(types.KeyboardButton(city))
     bot.reply_to(message, "Привет! Я бот, который дает информацию о погоде. Выбери город из списка ниже:", reply_markup=markup)
 
-
 @bot.message_handler(func=lambda message: message.text in cities)
 def get_weather(message):
     city = message.text
     weather_data = fetch_weather(city)
 
     if weather_data:
-        reply = f"Текущая погода в {city}: {weather_data['current']['condition']['text']}, температура {weather_data['current']['temp_c']}°C, ощущается как {weather_data['current']['feelslike_c']}°C."
-        bot.reply_to(message, reply)
+        weather_icon_code = weather_data['current']['condition']['icon']
+        weather_image_url = f'http:{weather_icon_code}'
+        photo = requests.get(weather_image_url)
+
+        with open('weather_image.png', 'wb') as photo_file:
+            photo_file.write(photo.content)
+
+        with open('weather_image.png', 'rb') as photo_file:
+            bot.send_photo(message.chat.id, photo_file, caption=f"Текущая погода в {city}: {weather_data['current']['condition']['text']}, температура {weather_data['current']['temp_c']}°C, ощущается как {weather_data['current']['feelslike_c']}°C.")
     else:
         bot.reply_to(message, "Извините, не удалось получить информацию о погоде для этого города.")
-
 
 def fetch_weather(city):
     url = f'http://api.weatherapi.com/v1/current.json?key={API_KEY}&q={city}&lang=ru'
@@ -40,5 +44,7 @@ def fetch_weather(city):
     else:
         return None
 
+
 bot.polling()
+
 
