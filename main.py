a5756e62177f777911f3183da237f9489875d29e
Jone <eugeni.zhidkov@yandex.ru>
2024-04-29 20:08:05 +1000

    восьмой

--- main.py
+++ main.py
@@ -1,17 +1,36 @@
 #7138618116:AAHMWU4rmmD-W48pIecOrKBxAl6z6D4ZEbI
 #4de6ee8e61214f0182e130103242804
-
+#self.cities_en = {}
+#self.cities_en["Москва"] = "Moscow"
+#self.cities_en["Лондон"] = "London"
+#self.cities_en["Париж"] = "Paris"
+#self.cities_en["Берлин"] = "Berlin"
+#self.cities_en["Нью-Йорк"] = "New-York"
+#self.cities_en["Токио"] = "Tokyo"
+#18bb59ab-59c0-46a4-889f-24eca41a104f
 import telebot
 from telebot import types
 import requests
+from datetime import datetime, timedelta
+import matplotlib.pyplot as plt
 
-API_KEY = '4de6ee8e61214f0182e130103242804'
+TELEGRAM_BOT_TOKEN = '7138618116:AAHMWU4rmmD-W48pIecOrKBxAl6z6D4ZEbI'
+WEATHER_API_KEY = '4de6ee8e61214f0182e130103242804'
 
 class WeatherBot:
     def __init__(self, token):
         self.bot = telebot.TeleBot(token)
         self.cities = ["Москва", "Лондон", "Париж", "Берлин", "Нью-Йорк", "Токио"]
+        self.cities_en = {
+            "Москва": "Moscow",
+            "Лондон": "London",
+            "Париж": "Paris",
+            "Берлин": "Berlin",
+            "Нью-Йорк": "New York",
+            "Токио": "Tokyo"
+        }
         self.registered_users_file = 'registered_users.txt'
+        self.feedback_file = 'feedback.txt'
 
     def start(self):
         @self.bot.message_handler(commands=['start'])
@@ -31,8 +50,9 @@ class WeatherBot:
             if self.is_registered(user_id):
                 city = message.text
                 weather_data = self.fetch_weather(city)
+                local_time_data = self.get_local_time(city)
 
-                if weather_data:
+                if weather_data and local_time_data:
                     weather_icon_code = weather_data['current']['condition']['icon']
                     weather_image_url = f'http:{weather_icon_code}'
                     photo = requests.get(weather_image_url)
@@ -41,7 +61,20 @@ class WeatherBot:
                         photo_file.write(photo.content)
 
                     with open('weather_image.png', 'rb') as photo_file:
-                        self.bot.send_photo(message.chat.id, photo_file, caption=f"Текущая погода в {city}: {weather_data['current']['condition']['text']}, температура {weather_data['current']['temp_c']}°C, ощущается как {weather_data['current']['feelslike_c']}°C.")
+                        weather_caption = f"<b>Текущая погода в {city}:</b>\n{weather_data['current']['condition']['text']}, температура {weather_data['current']['temp_c']}°C, ощущается как {weather_data['current']['feelslike_c']}°C, скорость ветра {weather_data['current']['wind_kph']} км/ч."
+                        local_time_caption = f"<b>Местное время в {city}:</b>\n{self.format_local_time(local_time_data['datetime'])}"
+                        clothing_advice = self.get_clothing_advice(weather_data['current']['temp_c'])
+
+                        self.bot.send_photo(message.chat.id, photo_file, caption=f"{weather_caption}\n\n{local_time_caption}\n\n<b>Совет по одежде:</b>\n{clothing_advice}", parse_mode='HTML')
+
+                        # Добавляем график температуры на 5 дней
+                        forecast_data = self.fetch_forecast(city)
+                        if forecast_data:
+                            self.plot_temperature_forecast(forecast_data)
+                            with open('temperature_forecast.png', 'rb') as forecast_file:
+                                self.bot.send_photo(message.chat.id, forecast_file, caption="Прогноз температуры на 5 дней")
+                        else:
+                            self.bot.reply_to(message, "Извините, не удалось получить прогноз погоды.")
                 else:
                     self.bot.reply_to(message, "Извините, не удалось получить информацию о погоде для этого города.")
             else:
@@ -64,6 +97,18 @@ class WeatherBot:
                 self.bot.reply_to(message, "Ваш аккаунт успешно удален.")
             else:
                 self.bot.reply_to(message, "Ваш аккаунт не найден.")
+
+        @self.bot.message_handler(commands=['feedback'])
+        def feedback_command(message):
+            self.bot.reply_to(message, "Напишите ваш отзыв о боте.")
+
+        @self.bot.message_handler(func=lambda message: True)
+        def handle_message(message):
+            if message.text == '/feedback':
+                self.bot.reply_to(message, "Напишите ваш отзыв о боте.")
+            else:
+                self.save_feedback(message)
+                self.bot.reply_to(message, "Спасибо за ваш отзыв! Он был сохранен.")
 
         self.bot.polling()
 
@@ -88,14 +133,75 @@ class WeatherBot:
                     file.write(line)
 
     def fetch_weather(self, city):
-        url = f'http://api.weatherapi.com/v1/current.json?key={API_KEY}&q={city}&lang=ru'
+        url = f'http://api.weatherapi.com/v1/current.json?key={WEATHER_API_KEY}&q={city}&lang=ru'
         response = requests.get(url)
         if response.status_code == 200:
             return response.json()
         else:
             return None
 
-if __name__ == "__main__":
-    bot = WeatherBot('7138618116:AAHMWU4rmmD-W48pIecOrKBxAl6z6D4ZEbI')
-    bot.start()
+    def get_local_time(self, city):
+        if city == "Нью-Йорк":
+            url = 'http://worldtimeapi.org/api/timezone/America/New_York?lang=ru'
+        elif city == "Токио":
+            url = 'http://worldtimeapi.org/api/timezone/Asia/Tokyo?lang=ru'
+        else:
+            url = f'http://worldtimeapi.org/api/timezone/Europe/{self.cities_en[city]}?lang=ru'
+        response = requests.get(url)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            return None
 
+    def format_local_time(self, local_time):
+        time_obj = datetime.strptime(local_time, '%Y-%m-%dT%H:%M:%S.%f%z')
+        formatted_time = time_obj.strftime('%d.%m.%Y %H:%M:%S')
+        return formatted_time
+
+    def get_clothing_advice(self, temperature):
+        if temperature < -10:
+            return "На улицу лучше выйти в теплой куртке, шапке, шарфе и перчатках."
+        elif -10 <= temperature < 0:
+            return "На улицу лучше выйти в зимней куртке, шарфе и перчатках."
+        elif 0 <= temperature < 10:
+            return "На улицу лучше выйти в осенней куртке."
+        elif 10 <= temperature < 20:
+            return "На улицу лучше выйти в свитере или кофте."
+        elif 20 <= temperature < 30:
+            return "На улицу лучше выйти в футболке или майке."
+        else:
+            return "На улицу лучше выйти в легкой одежде, возможно в шортах и футболке."
+
+    def fetch_forecast(self, city):
+        url = f'http://api.weatherapi.com/v1/forecast.json?key={WEATHER_API_KEY}&q={city}&days=5&lang=ru'
+        response = requests.get(url)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            return None
+
+    def plot_temperature_forecast(self, forecast_data):
+        dates = []
+        temps = []
+        for day in forecast_data['forecast']['forecastday']:
+            date = datetime.strptime(day['date'], '%Y-%m-%d').date()
+            temp = day['day']['avgtemp_c']
+            dates.append(date)
+            temps.append(temp)
+        plt.figure(figsize=(8, 5))
+        plt.plot(dates, temps, marker='o', linestyle='-')
+        plt.title('Прогноз температуры на 5 дней')
+        plt.xlabel('Дата')
+        plt.ylabel('Температура, °C')
+        plt.grid(True)
+        plt.tight_layout()
+        plt.savefig('temperature_forecast.png')
+
+    def save_feedback(self, message):
+        feedback = message.text
+        with open(self.feedback_file, 'a', encoding='utf-8') as file:
+            file.write(feedback + '\n')
+
+if __name__ == "__main__":
+    bot = WeatherBot(TELEGRAM_BOT_TOKEN)
+    bot.start()
